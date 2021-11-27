30ee93c7b1aba14f6cc326fd82886d3a969cb014
Igor Izrailev <izraigo@gmail.com>
2021-11-27 15:35:07 +0300

    little changes

--- renderer.js
+++ renderer.js
@@ -1,10 +1,3 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
-
 const NO3 = 0;
 const NH4 = 1;
 const P   = 2;
@@ -16,7 +9,7 @@ const ELEMENTS_NO   = 7;
 
 const elements  = ['NH4', 'NO3', 'P', 'K', 'Ca', 'Mg', 'S'];
 
-const profile = [12.43, 165.7, 60, 226.840, 141.83, 32.25, 42.543];
+let profile = [12.428, 165.702, 60, 226.840, 141.83, 32.25, 42.543];
 updateProfileInputs();
 
 let fCaN2O6    = [    0.7,  14.176,         0,       0,  19.28,      0,      0];
@@ -27,21 +20,54 @@ let fKH2PO4    = [      0,       0,      21.8,  27.518,      0,      0,      0];
 let fK2SO4     = [      0,       0,         0,  44.874,      0,      0, 18.401];
 
 const fertilisersPercent = math.matrixFromRows(fCaN2O6, fKNO3, fNH4NO3, fMgSO4, fKH2PO4, fK2SO4);
-const fertilisers = math.multiply(fertilisersPercent, 0.01);
-const fertilisersInv = math.inv(math.transpose(math.resize(fertilisers, [6,6], 0)));
+const fertilisers = math.transpose(math.multiply(fertilisersPercent, 0.01));
+const mInvFreeS  = math.inv(fertilisers.filter((value, index, arr) => index != S));
+const mInvFreeCa = math.inv(fertilisers.filter((value, index, arr) => index != Ca));
+
 
 updateFertilisersInputs();
-onProfileChange();
 
+let no3 = parseFloat(document.getElementById('pNO3').value);
+let nh4 = parseFloat(document.getElementById('pNH4').value);
+console.log(no3 + " " + nh4);
+document.getElementById('pN').value = nh4 + no3;
+document.getElementById('pNRatio').value = (nh4 / no3).toFixed(3);
+onProfileChangeFreeS();
 
+document.getElementById('pN').addEventListener('input', e => {onNchange()});
+document.getElementById('pNRatio').addEventListener('input', e => {onNchange()});
+document.getElementById('pNO3').addEventListener('input', e => {onNO3change()});
+document.getElementById('pNH4').addEventListener('input', e => {onNH4change()});
+document.getElementById('pP').addEventListener('input', e => {onProfileChangeFreeS()});
+document.getElementById('pK').addEventListener('input', e => {onProfileChangeFreeS()});
+document.getElementById('pCa').addEventListener('input', e => {onProfileChangeFreeS()});
+document.getElementById('pMg').addEventListener('input', e => {onProfileChangeFreeS()});
+document.getElementById('pS').addEventListener('input', e => {onProfileChangeFreeCa()});
 
-for (let i = 0; i < ELEMENTS_NO; i++) {
-    document.getElementById('pr' + elements[i]).addEventListener('input', e => {onProfileChange()});
+function onProfileChangeFreeS() {
+    solver = () => math.multiply(mInvFreeS, profile.filter((value, index, arr) => index != S));
+    onProfileChange(solver);
 }
 
-function onProfileChange() {
-    updateProfileMatrix();
+function onProfileChangeFreeCa() {
+    solver = () => math.multiply(mInvFreeCa, profile.filter((value, index, arr) => index != Ca));
+    onProfileChange(solver);
+}
 
+function onProfileChange(solver) {
+    updateProfileMatrix();
+    updateRatios();
+    let weights = solver();
+    for (let w = 1; w < ELEMENTS_NO; w++) {
+        document.getElementById('w' + w).textContent = (weights[w - 1] * 0.001).toFixed(3);
+    }
+
+    profile = math.multiply(fertilisers, weights);
+    updateProfileInputs();
+}
+
+
+function updateRatios() {
     let p = [parseFloat(profile[NO3]) + parseFloat(profile[NH4]), profile[P], profile[K], profile[Ca], profile[Mg], profile[S]];
     let m = math.matrixFromRows(p, p, p, p, p, p);
     let m2 = math.matrixFromColumns(p, p, p, p, p, p);
@@ -52,36 +78,52 @@ function onProfileChange() {
             document.getElementById('r' + r + c).textContent = ratios[r - 1][c - 1].toFixed(3);
         }
     }
-
-    weights = math.multiply(fertilisersInv, math.multiply(math.resize(profile, [6], 0), 0.001));
-    for (let w = 1; w < ELEMENTS_NO; w++) {
-        document.getElementById('w' + w).textContent = weights[w - 1].toFixed(3);
-    }
 }
 
-function combineN() {
-    
-    for (let i = 0; i < ELEMENTS_NO; i++) {
-        profile[i] = document.getElementById('pr' + elements[i]).value;    
-    }
-    
+function onNchange() {
+    let ratio = parseFloat(document.getElementById('pNRatio').value);
+    let n = parseFloat(document.getElementById('pN').value);
+    nh4 = (n / (1 + 1 / ratio))
+    no3 = n - nh4;
+    document.getElementById('pNH4').value = nh4.toFixed(3);
+    document.getElementById('pNO3').value = no3.toFixed(3);
+    onProfileChangeFreeS();
+}
+
+function onNH4change() {
+    let nh4 = parseFloat(document.getElementById('pNH4').value);
+    let n = parseFloat(document.getElementById('pN').value);
+    let no3 = n - nh4
+    document.getElementById('pNO3').value = no3;
+    document.getElementById('pNRatio').value = (nh4 / no3).toFixed(3);
+    onProfileChangeFreeS();
+}
+
+function onNO3change() {
+    let no3 = parseFloat(document.getElementById('pNO3').value);
+    let n = parseFloat(document.getElementById('pN').value);
+    let nh4 = n - no3
+    document.getElementById('pNH4').value = nh4;
+    document.getElementById('pNRatio').value = (nh4 / no3).toFixed(3);
+    onProfileChangeFreeS();
 }
 
 function updateProfileMatrix() {
     for (let i = 0; i < ELEMENTS_NO; i++) {
-        profile[i] = document.getElementById('pr' + elements[i]).value;    
+        profile[i] = parseFloat(document.getElementById('p' + elements[i]).value);    
     }
 }
 
 function updateProfileInputs() {
     for (let i = 0; i < ELEMENTS_NO; i++) {
-        document.getElementById('pr' + elements[i]).value = profile[i];    
+        document.getElementById('p' + elements[i]).value = profile[i].toFixed(3);
     }
+    document.getElementById('pN').value = (profile[NH4] + profile[NO3]).toFixed(3);
 }
 
 function updateFertilisersInputs() {
     for (let r = 1; r < ELEMENTS_NO; r++) {
-      for (let c = 1; c < ELEMENTS_NO; c++) {
+      for (let c = 1; c <= ELEMENTS_NO; c++) {
           document.getElementById('f' + r + c).textContent = fertilisersPercent[r-1][c-1].toFixed(3);    
       }
     }
